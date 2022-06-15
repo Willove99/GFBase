d0902170d07132c0e7cf9aa6bead790b9acb8b85
Will <adrianwhp@163.com>
2022-06-15 14:27:44 +0800

    Initial commit

---
+++ Example/GFBase/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  GFBase
+//
+//  Created by Will on 06/15/2022.
+//  Copyright (c) 2022 Will. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+}
+
